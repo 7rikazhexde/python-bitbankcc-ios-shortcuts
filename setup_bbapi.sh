4ac09ffac010a97bc07513f238ff91a43f55b96d
7rikaz_h785 <7rikaz.h785.stat2ltas41lcijad@gmail.com>
2023-06-23 01:03:24 +0900

    feat: Change pip install command

--- setup_bbapi.sh
+++ setup_bbapi.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+# venvかpyenv-virtualenvで仮想環境を作成してください。
+
 # リポジトリをクローン
-git clone https://github.com/bitbankinc/python-bitbankcc.git
+# git clone https://github.com/bitbankinc/python-bitbankcc.git
 
 # クローンしたリポジトリのディレクトリに移動
-cd python-bitbankcc
+# cd python-bitbankcc
 
 # 最新のコミットに切り替え
-git checkout HEAD
+# git checkout HEAD
 
 # パッケージをインストール
-pip install .
+pip install git+https://github.com/bitbankinc/python-bitbankcc#egg=python-bitbankcc
 pip install python-dotenv
 pip install requests
 
 # 一つ上のディレクトリに移動
-cd ..
+# cd ..
 
 # 設定ファイル作成
-sh setup_config.sh
+sh ./setup_config.sh
 
 # python-bitbankccフォルダを削除
-rm -rf python-bitbankcc
+# rm -rf python-bitbankcc
